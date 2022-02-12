ac7e5ee3375e9deb5cdfb80a7999f637a2171aef
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2022-02-12 17:35:12 +0900

    URLにクエリストリングがついても動作するように修正

--- config/routes.php
+++ config/routes.php
@@ -10,18 +10,31 @@ class Route {
 
     /**
      * URLから対応するfilePathを返す
-     * @param string $path アクセスパス
      * @return string 返すパス
      */
-   public static function resolveFile($path):string
+   public static function resolveFile():string
    {
        $map = self::ROUTE_MAP;
+       $path = self::parseUrl();
        foreach ($map as $info) {
            if($path === $info['path']) {
                return $info['file'];
            }
        }
        return 'src/notFound.php';
+   }
+
+   private static function parseUrl():string{
+       if(!isset($_SERVER['REQUEST_URI'])){
+           throw new RuntimeException('$_SERVER["REQUEST_URI"]が定義されていません');
+       }
+       $path = $_SERVER['REQUEST_URI'];
+       $position = strpos($path,'?');
+       if($position === false){
+           return $path;
+       } else {
+           return (explode('?', $path))[0];
+       }
    }
 
 
